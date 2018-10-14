c862f2db7746897746d9cf1c4d2bba8347d11002
Hong Duc <01627dlm@gmail.com>
2018-10-14 17:44:02 +0700

    Khoi dong project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNhanVien
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            frm_Login frm = new frm_Login();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new Form2());
+            }
+        }
+    }
+}
